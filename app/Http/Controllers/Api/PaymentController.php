314fba2399dfc59f06a6a74d40fde02a9782623b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-05-07 17:50:41 +0700

    Halaman Pembayaran List Pelanggan Filter By Bulan Ini Sudah Bayar

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
+use App\Models\Customer;
+use App\Http\Requests\Payment\GetPerCustomerForCurrentMonthRequest;
 use DataTables;
 
 class PaymentController extends Controller
 {
     
-    public function getAll() {
-        return DataTables::of(Payment::with('pelanggan'))->toJson();
+    /**
+     * perCustomerForCurrentMonth
+     *
+     * @return void
+     */
+    public function perCustomerForCurrentMonth(GetPerCustomerForCurrentMonthRequest $request) {
+        $customerQuery = Customer::when($request->has('is_paid'), function ($query) use ($request) {
+            $isPaid = $request->get('is_paid');
+            
+            if ($isPaid) {
+                $query->isPaidForCurrentMonth();
+            } else {
+                $query->isNotPaidForCurrentMonth();
+            }
+        });
+
+        return Datatables::of($customerQuery)->toJson();
     }
 
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -22,4 +22,35 @@ class Customer extends Model
      * @var array
      */
     protected $fillable = ['nama', 'no_telp'];
+    
+    /**
+     * payment
+     *
+     * @return void
+     */
+    public function payment() {
+        return $this->hasMany(Payment::class, 'pelanggan_id');
+    }
+    
+    /**
+     * scopeIsPaidForCurrentMonth
+     *
+     * @return void
+     */
+    public function scopeIsPaidForCurrentMonth($query) {
+        $query->whereHas('payment', function ($query) {
+            $query->whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()]);
+        });
+    }
+    
+    /**
+     * scopeIsNotPaidForCurrentMonth
+     *
+     * @return void
+     */
+    public function scopeIsNotPaidForCurrentMonth($query) {
+        $query->whereDoesntHave('payment', function ($query) {
+            $query->whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()]);
+        });
+    }
 }
