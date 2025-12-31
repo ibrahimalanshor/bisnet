5b1f9a0138f889459c48734e3cf501771b974ac3
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-31 13:56:40 +0700

    setup project

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import vClickOutside from 'click-outside-vue3';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
+
+createApp(App).use(pinia).use(vClickOutside).mount('#app');
