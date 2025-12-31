054b9921ba139d0d77f50a257947e5df13b8fa8d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-31 14:08:47 +0700

    reset

--- resources/views/components/sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach ($menus as $menu)
-    <li class="nav-item @if(request()->routeIs($menu['active_route'])) active @endif">
-        <a class="nav-link" href="{{ route($menu['route']) }}">
-            <i class="fas fa-fw fa-{{ $menu['icon'] }}"></i>
-            <span>{{ $menu['name'] }}</span></a>
-    </li>
-@endforeach
