cc52bbe69320afb432cc42cd986b17b55781424c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-01-06 16:30:29 +0700

    base pagination

---
+++ src/components/base/BasePagination.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps({
+  sideCount: {
+    type: Number,
+    default: 1,
+  },
+  totalPages: Number,
+});
+const currentPage = defineModel();
+
+const pages = computed(() => {
+  let left = Math.max(1, currentPage.value - props.sideCount);
+
+  if (left - 1 === 2) {
+    left--;
+  }
+
+  let right = Math.min(currentPage.value + props.sideCount, props.totalPages);
+
+  if (props.totalPages - right === 2) {
+    right++;
+  }
+
+  return Array.from({ length: right - left }, (_, i) => i + 1);
+});
+</script>
+
+<template>
+  <nav>{{ pages }}</nav>
+</template>
