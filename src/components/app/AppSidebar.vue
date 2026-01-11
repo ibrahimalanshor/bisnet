354a83eabdf5154bedbcec81e1f7c120be4794ef
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-01-11 19:13:53 +0700

    barang children menu

---
+++ src/components/app/AppSidebar.vue
@@ -0,0 +1,164 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+import { useRoute, useRouter } from 'vue-router';
+import { h, ref } from 'vue';
+
+const router = useRouter();
+const route = useRoute();
+
+const menus = [
+  {
+    id: 'dashboard',
+    to: { name: 'dashboard' },
+    name: 'Dashboard',
+    icon: 'ri:dashboard-2-line',
+  },
+  {
+    id: 'supplier',
+    to: { name: 'supplier' },
+    name: 'Supplier',
+    icon: 'ri:truck-line',
+  },
+  {
+    id: 'product',
+    name: 'Barang',
+    icon: 'ri:archive-2-line',
+    children: [
+      {
+        id: 'product',
+        to: { name: 'product' },
+        name: 'Kelola Barang',
+      },
+      {
+        id: 'category_product',
+        to: { name: 'product' },
+        name: 'Kategori Barang',
+      },
+    ],
+  },
+  {
+    id: 'restock',
+    to: { name: 'restock.index' },
+    activeKey: ['restock.index', 'restock.new'],
+    name: 'Restock',
+    icon: 'ri:box-3-line',
+  },
+  {
+    id: 'sale',
+    to: { name: 'sale.index' },
+    activeKey: ['sale.index', 'sale.new'],
+    name: 'Penjualan',
+    icon: 'ri:calculator-line',
+  },
+];
+
+const sidebarVisible = defineModel();
+const menusOpened = ref(
+  menus
+    .filter(
+      (menu) =>
+        menu.children && menu.children.length && checkMenuIsActive(menu),
+    )
+    .map((menu) => menu.id),
+);
+
+const classList = {
+  base: 'rounded-md px-3.5 py-2.5 flex items-center transition',
+  active: 'bg-blue-600 font-medium',
+  activeChild: 'bg-gray-800 font-medium',
+  inactive: 'text-gray-400 hover:bg-gray-800 hover:text-white',
+};
+
+function checkMenuIsActive(menu) {
+  if (!menu.activeKey) {
+    return menu.id === route.name;
+  }
+
+  return menu.activeKey.includes(route.name);
+}
+
+function onClickOutsideSidebar(e) {
+  const toggleSidebarButton = document.querySelector('#toggle-sidebar-button');
+
+  if (!toggleSidebarButton.contains(e.target)) {
+    sidebarVisible.value = false;
+  }
+}
+function onToggleMenu(menu) {
+  if (menusOpened.value.includes(menu.id)) {
+    menusOpened.value = menusOpened.value.filter((menu) => menu.id !== menu.id);
+  } else {
+    menusOpened.value.push(menu.id);
+  }
+}
+
+const MenuIcon = ({ icon }) =>
+  h('div', { class: 'w-7' }, h(Icon, { class: 'size-4', icon }));
+
+router.beforeEach(() => {
+  sidebarVisible.value = false;
+});
+</script>
+
+<template>
+  <aside
+    :class="[
+      'z-10 bg-gray-900 text-white h-screen w-72 fixed top-0 left-0 px-4 py-6 flex flex-col gap-1 transition lg:translate-x-0',
+      sidebarVisible ? 'translate-x-0' : '-translate-x-full',
+    ]"
+    v-click-outside="onClickOutsideSidebar"
+  >
+    <template v-for="menu in menus" :key="menu.id">
+      <router-link
+        v-if="!menu.children || !menu.children.length"
+        :to="menu.to"
+        :class="[
+          classList.base,
+          checkMenuIsActive(menu) ? classList.active : classList.inactive,
+        ]"
+      >
+        <MenuIcon :icon="menu.icon" />
+        {{ menu.name }}
+      </router-link>
+
+      <template v-else>
+        <div
+          :class="[
+            classList.base,
+            checkMenuIsActive(menu) ? classList.active : classList.inactive,
+            'justify-between cursor-pointer',
+          ]"
+          @click="onToggleMenu(menu)"
+        >
+          <div class="flex items-center">
+            <MenuIcon :icon="menu.icon" />
+            {{ menu.name }}
+          </div>
+          <Icon
+            :icon="
+              menusOpened.includes(menu.id)
+                ? 'ri:arrow-down-s-line'
+                : 'ri:arrow-right-s-line'
+            "
+          />
+        </div>
+
+        <div v-if="menusOpened.includes(menu.id)" v-motion-fade>
+          <router-link
+            v-for="child in menu.children"
+            :key="child.id"
+            :to="child.to"
+            :class="[
+              classList.base,
+              checkMenuIsActive(child)
+                ? classList.activeChild
+                : classList.inactive,
+            ]"
+          >
+            <span class="ml-7">{{ child.name }}</span>
+          </router-link>
+        </div>
+      </template>
+    </template>
+  </aside>
+</template>
--- src/components/layouts/LayoutAdmin.vue
+++ src/components/layouts/LayoutAdmin.vue
@@ -2,69 +2,12 @@
 import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
 import BaseAlert from '../base/BaseAlert.vue';
+import AppSidebar from '../app/AppSidebar.vue';
 import { useToastStore } from '../../cores/toast/toast.store';
-import { useRoute, useRouter } from 'vue-router';
 
 const toastStore = useToastStore();
-const router = useRouter();
-const route = useRoute();
-
-const menus = [
-  {
-    id: 'dashboard',
-    to: { name: 'dashboard' },
-    name: 'Dashboard',
-    icon: 'ri:dashboard-2-line',
-  },
-  {
-    id: 'supplier',
-    to: { name: 'supplier' },
-    name: 'Supplier',
-    icon: 'ri:truck-line',
-  },
-  {
-    id: 'product',
-    to: { name: 'product' },
-    name: 'Barang',
-    icon: 'ri:archive-2-line',
-  },
-  {
-    id: 'restock',
-    to: { name: 'restock.index' },
-    activeKey: ['restock.index', 'restock.new'],
-    name: 'Restock',
-    icon: 'ri:box-3-line',
-  },
-  {
-    id: 'sale',
-    to: { name: 'sale.index' },
-    activeKey: ['sale.index', 'sale.new'],
-    name: 'Penjualan',
-    icon: 'ri:calculator-line',
-  },
-];
 
 const sidebarVisible = ref(false);
-
-function checkMenuIsActive(menu) {
-  if (!menu.activeKey) {
-    return menu.id === route.name;
-  }
-
-  return menu.activeKey.includes(route.name);
-}
-
-function onClickOutsideSidebar(e) {
-  const toggleSidebarButton = document.querySelector('#toggle-sidebar-button');
-
-  if (!toggleSidebarButton.contains(e.target)) {
-    sidebarVisible.value = false;
-  }
-}
-
-router.beforeEach(() => {
-  sidebarVisible.value = false;
-});
 </script>
 
 <template>
@@ -82,30 +25,9 @@ router.beforeEach(() => {
         >{{ toast.message }}</BaseAlert
       >
     </div>
-    <aside
-      :class="[
-        'z-10 bg-gray-900 text-white h-screen w-72 fixed top-0 left-0 px-4 py-6 flex flex-col gap-1 transition lg:translate-x-0',
-        sidebarVisible ? 'translate-x-0' : '-translate-x-full',
-      ]"
-      v-click-outside="onClickOutsideSidebar"
-    >
-      <router-link
-        v-for="menu in menus"
-        :key="menu.id"
-        :to="menu.to"
-        :class="[
-          'rounded-md px-3.5 py-2.5 flex items-center transition',
-          checkMenuIsActive(menu)
-            ? 'bg-blue-600 font-medium'
-            : 'text-gray-400 hover:bg-gray-800 hover:text-white',
-        ]"
-      >
-        <div class="w-7">
-          <Icon :icon="menu.icon" class="size-4" />
-        </div>
-        {{ menu.name }}
-      </router-link>
-    </aside>
+
+    <AppSidebar v-model="sidebarVisible" />
+
     <div class="lg:ml-72">
       <nav
         class="h-14 bg-white px-4 flex items-center border-b border-gray-300 justify-between lg:justify-end lg:h-16"
