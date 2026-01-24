f37961bd6207596c7eaa16f2e0dd1b35f94aac76
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-01-24 18:11:19 +0700

    sale report summary

--- src/features/report/components/ReportSalePage.vue
+++ src/features/report/components/ReportSalePage.vue
@@ -4,17 +4,36 @@ import BaseCard from '../../../components/base/BaseCard.vue';
 import BaseFormItem from '../../../components/base/BaseFormItem.vue';
 import BaseInput from '../../../components/base/BaseInput.vue';
 import BaseButton from '../../../components/base/BaseButton.vue';
-import { reactive } from 'vue';
+import BaseDescriptionList from '../../../components/base/BaseDescriptionList.vue';
+import { reactive, ref } from 'vue';
+import { formatCurrency } from '../../../utils/common.js';
 
 const filter = reactive({
   date: null,
+});
+
+const columns = [
+  {
+    id: 'total_transactions',
+    name: 'Total Transaksi',
+    value: (item) => formatCurrency(item.total_transactions),
+  },
+  {
+    id: 'total_sales',
+    name: 'Total Penjualan',
+    value: (item) => formatCurrency(item.total_sales),
+  },
+];
+const summary = ref({
+  total_transactions: 29,
+  total_sales: 2582900,
 });
 </script>
 
 <template>
   <BaseHeading>Laporan Penjualan</BaseHeading>
 
-  <BaseCard title="Laporan Penjualan">
+  <BaseCard title="Form Laporan Penjualan">
     <form class="space-y-4">
       <BaseFormItem id="report_sale_form.date" label="Tanggal" v-slot="{ id }">
         <BaseInput type="date" :id="id" required v-model="filter.date" />
@@ -30,5 +49,13 @@ const filter = reactive({
         >Tampilkan</BaseButton
       >
     </form>
+  </BaseCard>
+
+  <BaseCard title="Laporan Penjualan 22 Desember 2026">
+    <BaseDescriptionList
+      :columns="columns"
+      :data="summary"
+      class="sm:grid-cols-2"
+    ></BaseDescriptionList>
   </BaseCard>
 </template>
