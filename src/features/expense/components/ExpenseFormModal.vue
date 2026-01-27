af59acca24da9c1d74ce23b3328f6106e98639ea
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-01-27 14:02:12 +0700

    Expense form

--- src/features/expense/components/ExpenseFormModal.vue
+++ src/features/expense/components/ExpenseFormModal.vue
@@ -4,41 +4,37 @@ import BaseFormItem from '../../../components/base/BaseFormItem.vue';
 import BaseInput from '../../../components/base/BaseInput.vue';
 import BaseButton from '../../../components/base/BaseButton.vue';
 import BaseAlert from '../../../components/base/BaseAlert.vue';
-import BaseSkeleton from '../../../components/base/BaseSkeleton.vue';
 import { ref, reactive } from 'vue';
-import { sleep } from '../../../utils/common.js';
+import { sleep, currencyToNum } from '../../../utils/common.js';
 import { useToastStore } from '../../../cores/toast/toast.store.js';
 
-const props = defineProps({
-  id: null,
-});
 const visible = defineModel('visible');
 const emit = defineEmits(['saved']);
 
 const toastStore = useToastStore();
-const loadingForm = ref(false);
 const loadingSave = ref(false);
 const error = ref(false);
 const form = reactive({
   name: null,
+  amount: null,
+  originalAmount: null,
 });
-
-async function loadForm() {
-  loadingForm.value = true;
-
-  await sleep();
-
-  loadingForm.value = false;
-}
 
 function onOpen() {
   loadingSave.value = false;
   error.value = false;
 
   form.name = null;
+  form.amount = null;
+  form.originalAmount = null;
+}
+function onChangeAmount() {
+  const amount = currencyToNum(form.amount);
 
-  if (props.id) {
-    loadForm();
+  if (amount < 1) {
+    form.amount = form.originalAmount;
+  } else {
+    form.originalAmount = form.amount;
   }
 }
 async function onSubmit() {
@@ -47,9 +43,7 @@ async function onSubmit() {
   await sleep();
 
   toastStore.create({
-    message: props.id
-      ? 'Berhasil memperbarui kategori'
-      : 'Berhasil menambahkan kategori baru',
+    message: 'Berhasil menambah pengeluaran',
     type: 'success',
   });
   visible.value = false;
@@ -62,24 +56,29 @@ async function onSubmit() {
 
 <template>
   <BaseModal
-    :title="props.id ? 'Edit Kategori' : 'Tambah Kategori'"
+    title="Tambah Pengeluaran"
     size="sm"
     v-model:visible="visible"
     @open="onOpen"
   >
-    <BaseSkeleton v-if="loadingForm" class="h-40" />
-    <form v-else class="space-y-4" @submit.prevent="onSubmit">
-      <BaseAlert v-if="error"> Gagal menyimpan kategori baru. </BaseAlert>
-      <BaseFormItem
-        id="product_category_form.name"
-        label="Nama"
-        v-slot="{ id }"
-      >
+    <form class="space-y-4" @submit.prevent="onSubmit">
+      <BaseAlert v-if="error"> Gagal menyimpan pengeluaran. </BaseAlert>
+      <BaseFormItem id="expense_form.name" label="Nama" v-slot="{ id }">
         <BaseInput
           :id="id"
-          placeholder="Makanan"
+          placeholder="Bayar Listrik"
           required
           v-model="form.name"
+        />
+      </BaseFormItem>
+      <BaseFormItem id="expense_form.amount" label="Nominal" v-slot="{ id }">
+        <BaseInput
+          :id="id"
+          currency
+          placeholder="125,000"
+          required
+          v-model="form.amount"
+          @change="onChangeAmount"
         />
       </BaseFormItem>
       <div class="flex gap-2 justify-end">
