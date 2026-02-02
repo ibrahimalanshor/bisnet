633f8ecdb2aebdd46694b6087b9a784093cfe9b7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-02-02 15:02:17 +0700

    auth token

--- src/features/auth/auth.store.js
+++ src/features/auth/auth.store.js
@@ -1,16 +1,22 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 
-export const useAuthStore = defineStore('auth', () => {
-  const loggedIn = ref(false);
-  const user = ref(null);
+export const useAuthStore = defineStore(
+  'auth',
+  () => {
+    const loggedIn = ref(false);
+    const user = ref(null);
+    const accessToken = ref(null);
 
-  const role = computed(() => user.value.role);
+    const role = computed(() => user.value.role);
 
-  function login(data) {
-    user.value = data.me;
-    loggedIn.value = true;
-  }
+    function login(data) {
+      user.value = data.me;
+      accessToken.value = data.token;
+      loggedIn.value = true;
+    }
 
-  return { loggedIn, user, role, login };
-});
+    return { loggedIn, user, role, accessToken, login };
+  },
+  { persist: true },
+);
