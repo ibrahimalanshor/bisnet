763ced933b9c919c2560be4b66709af6d5180985
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-02-09 15:25:33 +0700

    product chart

--- src/features/dashboard/components/ProductBarChart.vue
+++ src/features/dashboard/components/ProductBarChart.vue
@@ -2,32 +2,52 @@
 import { Bar } from 'vue-chartjs';
 import { ref } from 'vue';
 import { getChartColor } from '../../../utils/common';
+import BaseSkeleton from '../../../components/base/BaseSkeleton.vue';
+import { useRequest } from '../../../cores/http';
 
+const { request } = useRequest();
+
+const loading = ref(true);
 const data = ref({
-  labels: [
-    'Beras Premium 5kg',
-    'Minyak Goreng 1L',
-    'Gula Pasir 1kg',
-    'Telur Ayam Negeri',
-    'Mie Instan Goreng',
-    'Susu UHT 1L',
-    'Kopi Bubuk 200g',
-  ],
+  labels: [],
   datasets: [
     {
       label: 'Total Terjual',
       backgroundColor: getChartColor('red', 0.5),
       borderColor: getChartColor('red'),
       borderWidth: 2,
-      data: [120, 95, 80, 140, 200, 75, 90],
+      data: [],
     },
   ],
 });
 const options = {
   indexAxis: 'y',
+  scales: {
+    x: {
+      ticks: {
+        precision: 0,
+      },
+    },
+  },
 };
+
+async function loadData() {
+  loading.value = true;
+
+  const [res, err] = await request(`/api/v1/stats/products`);
+
+  if (!err) {
+    data.value.labels = res.map((item) => item.product_name);
+    data.value.datasets[0].data = res.map((item) => item.qty);
+  }
+
+  loading.value = false;
+}
+
+loadData();
 </script>
 
 <template>
-  <Bar :data="data" :options="options" />
+  <BaseSkeleton v-if="loading" class="min-h-50 h-full" />
+  <Bar v-else :data="data" :options="options" />
 </template>
