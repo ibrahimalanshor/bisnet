ee9c6c6a05112fe66727d9e99a76001780956fda
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2026-02-12 13:33:16 +0700

    report stock daily

--- src/features/report/components/ReportStockPage.vue
+++ src/features/report/components/ReportStockPage.vue
@@ -4,34 +4,38 @@ import BaseCard from '../../../components/base/BaseCard.vue';
 import BaseFormItem from '../../../components/base/BaseFormItem.vue';
 import BaseInput from '../../../components/base/BaseInput.vue';
 import BaseButton from '../../../components/base/BaseButton.vue';
+import BaseDescriptionList from '../../../components/base/BaseDescriptionList.vue';
 import BaseTable from '../../../components/base/BaseTable.vue';
-import BasePagination from '../../../components/base/BasePagination.vue';
 import BaseSelect from '../../../components/base/BaseSelect.vue';
 import BaseMonthSelect from '../../../components/base/BaseMonthSelect.vue';
 import BaseYearSelect from '../../../components/base/BaseYearSelect.vue';
+import BaseAlert from '../../../components/base/BaseAlert.vue';
 import { reactive, ref, computed } from 'vue';
 import {
   formatCurrency,
   formatDate,
-  sleep,
   getMonthNames,
 } from '../../../utils/common.js';
-import data from '../data/stock.json';
+import { useRequest } from '../../../cores/http.js';
 
+const { request } = useRequest();
 const months = getMonthNames();
 
-const tableColumns = [
-  { id: 'product', name: 'Barang', value: (item) => item.product_code },
+const summaryColumns = [
   {
     id: 'init_stock',
     name: 'Stok Awal',
     value: (item) => formatCurrency(item.init_stock),
   },
-  { id: 'income', name: 'Masuk', value: (item) => formatCurrency(item.income) },
   {
-    id: 'outcome',
-    name: 'Keluar',
-    value: (item) => formatCurrency(item.outocme),
+    id: 'total_income',
+    name: 'Total Masuk',
+    value: (item) => formatCurrency(item.total_income),
+  },
+  {
+    id: 'total_outcome',
+    name: 'Total Keluar',
+    value: (item) => formatCurrency(item.total_outcome),
   },
   {
     id: 'final_stock',
@@ -39,19 +43,54 @@ const tableColumns = [
     value: (item) => formatCurrency(item.final_stock),
   },
 ];
+const tableColumns = computed(() => {
+  return [
+    {
+      id: 'product_name',
+      name: 'Nama Barang',
+    },
+    {
+      id: 'init_stock',
+      name: 'Stok Awal',
+      value: (item) => formatCurrency(item.init_stock),
+    },
+    {
+      id: 'income',
+      name: 'Masuk',
+      value: (item) => formatCurrency(item.income),
+    },
+    {
+      id: 'outcome',
+      name: 'Keluar',
+      value: (item) => formatCurrency(item.outcome),
+    },
+    {
+      id: 'final_stock',
+      name: 'Stok Akhir',
+      value: (item) => formatCurrency(item.final_stock),
+    },
+  ];
+});
 
-const reports = ref(data.slice(0, 10));
+const summary = ref({
+  total_transactions: null,
+  total_sales: null,
+});
+const data = ref(null);
+const dataLoading = ref(false);
+
+const reports = computed(() => data.value.data);
 
 const resultVisible = ref(false);
-const loadingResult = ref(false);
+const resultError = ref(false);
+const resultLoading = ref(false);
+const resultTitle = ref(null);
+
 const filter = reactive({
   period: 'daily',
   date: null,
   month: 1,
   year: new Date().getFullYear(),
-});
-const query = reactive({
-  page: 1,
 });
 
 const formValid = computed(() => {
@@ -65,31 +104,59 @@ const formValid = computed(() => {
 
   return !!filter.year;
 });
-const title = computed(() => {
-  if (!formValid.value) {
-    return null;
-  }
-
-  if (filter.period === 'daily') {
-    return `Laporan Stok ${formatDate(filter.date, 'DD MMMM YYYY')}`;
-  }
-
-  if (filter.period === 'monthly') {
-    return `Laporan Stok Bulan ${months[filter.month]} ${filter.year}`;
-  }
-
-  return `Laporan Stok Tahun ${filter.year}`;
-});
 
 async function loadResult() {
-  loadingResult.value = true;
+  resultError.value = false;
+  resultLoading.value = true;
 
-  await sleep();
-  resultVisible.value = true;
+  const [, err] = await loadPeriodlyResult();
 
-  loadingResult.value = false;
+  if (err) {
+    resultError.value = true;
+  } else {
+    if (filter.period === 'daily') {
+      resultTitle.value = `Laporan Stok ${formatDate(filter.date, 'DD MMMM YYYY')}`;
+    } else if (filter.period === 'monthly') {
+      resultTitle.value = `Laporan Stok Bulan ${months[filter.month]} ${filter.year}`;
+    } else {
+      resultTitle.value = `Laporan Stok Tahun ${filter.year}`;
+    }
+
+    resultVisible.value = true;
+  }
+
+  resultLoading.value = false;
 }
+
+async function loadPeriodlyResult() {
+  dataLoading.value = true;
+
+  const [res, err] = await request(`/api/v1/reports/stocks`, {
+    query: {
+      period: filter.period,
+      date: filter.date,
+      month: filter.month,
+      year: filter.year,
+    },
+  });
+
+  if (!err) {
+    data.value = res;
+
+    summary.value.init_stock = res.meta.summary.init_stock;
+    summary.value.total_income = res.meta.summary.total_income;
+    summary.value.total_outcome = res.meta.summary.total_outcome;
+    summary.value.final_stock = res.meta.summary.final_stock;
+  }
+
+  dataLoading.value = false;
+
+  return [res, err];
+}
+
 function onChangePeriod() {
+  resultVisible.value = false;
+
   filter.date = null;
   filter.month = 1;
   filter.year = new Date().getFullYear();
@@ -99,10 +166,13 @@ function onChangePeriod() {
 <template>
   <BaseHeading>Laporan Stok</BaseHeading>
 
-  <BaseCard title="Form Laporan">
+  <BaseCard title="Form Laporan Stok">
     <form class="space-y-4" @submit.prevent="loadResult">
+      <BaseAlert v-if="resultError"
+        >Gagal menampilkan laporan, silakan coba lagi.</BaseAlert
+      >
       <BaseFormItem
-        id="report_stock_form.period"
+        id="report_sale_form.period"
         label="Periode"
         v-slot="{ id }"
       >
@@ -120,7 +190,7 @@ function onChangePeriod() {
       </BaseFormItem>
       <BaseFormItem
         v-if="filter.period === 'daily'"
-        id="report_stock_form.date"
+        id="report_sale_form.date"
         label="Tanggal"
         v-slot="{ id }"
       >
@@ -132,14 +202,14 @@ function onChangePeriod() {
       >
         <BaseFormItem
           v-if="filter.period === 'monthly'"
-          id="report_stock_form.month"
+          id="report_sale_form.month"
           label="Bulan"
           v-slot="{ id }"
         >
           <BaseMonthSelect :id="id" required v-model="filter.month" />
         </BaseFormItem>
         <BaseFormItem
-          id="report_stock_form.year"
+          id="report_sale_form.year"
           label="Tahun"
           :class="filter.period === 'yearly' ? 'col-span-2' : ''"
           v-slot="{ id }"
@@ -150,7 +220,7 @@ function onChangePeriod() {
       <BaseButton
         icon="ri:file-list-2-fill"
         :disabled="!formValid"
-        :loading="loadingResult"
+        :loading="resultLoading"
         >Tampilkan</BaseButton
       >
     </form>
@@ -158,7 +228,7 @@ function onChangePeriod() {
 
   <BaseCard
     v-if="resultVisible"
-    :title="title"
+    :title="resultTitle"
     :custom-class="{
       header:
         'flex-col items-start gap-2 md:flex-row md:items-center lg:flex-col lg:items-start xl:flex-row xl:items-center',
@@ -175,8 +245,17 @@ function onChangePeriod() {
       </div>
     </template>
     <div class="space-y-4">
-      <BaseTable :columns="tableColumns" :data="reports"></BaseTable>
-      <BasePagination :total-pages="10" v-model="query.page" />
+      <BaseDescriptionList
+        :columns="summaryColumns"
+        :data="summary"
+        class="sm:grid-cols-2 xl:grid-cols-4"
+      ></BaseDescriptionList>
+
+      <BaseTable
+        :loading="dataLoading"
+        :columns="tableColumns"
+        :data="reports"
+      ></BaseTable>
     </div>
   </BaseCard>
 </template>
